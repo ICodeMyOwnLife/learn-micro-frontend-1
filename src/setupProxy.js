90c6b316fd12686dc5a3864e49cd0d5ec2317fee
HienTM <hientm@sendo.vn>
2020-05-08 14:25:06 +0700

    Add

--- src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-module.exports = app => {
-  app.use((req, res, next) => {
-    console.log(
-      `remote: ${req.connection.remoteAddress}:${req.connection.remotePort} - originalUrl: ${req.originalUrl}`,
-    );
-    res.header('Access-Control-Allow-Origin', '*');
-    setTimeout(next, 500);
-  });
-};
