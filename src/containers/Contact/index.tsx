4c5dd6507394d35e56203d0634f9b2b5bb30e74c
HienTM <hientm@sendo.vn>
2020-05-22 15:09:21 +0700

    Add env

--- src/containers/Contact/index.tsx
+++ src/containers/Contact/index.tsx
@@ -1,12 +1,34 @@
-import React, { FC, memo } from 'react';
+import React, { FC, memo, useState } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
+import { KeyboardDateTimePicker, Button } from 'sp-ops-react-ui';
+import { datetime } from 'sp-datetime-utils';
 
-export const ContactComponent: FC<RouteComponentProps> = ({ location }) => (
-  <div>
-    <h2>Contact {process.env.REACT_APP_MF_CODE}</h2>
-    <p>{location.pathname}</p>
-  </div>
-);
+console.log(datetime);
+
+export const ContactComponent: FC<RouteComponentProps> = ({ location }) => {
+  const [time, setTime] = useState<datetime.Dayjs | null>(datetime());
+
+  return (
+    <div>
+      <h2>Contact {process.env.REACT_APP_MF_CODE}</h2>
+      <p>{location.pathname}</p>
+
+      <form>
+        <div>
+          <KeyboardDateTimePicker value={time} onChange={t => setTime(t)} />
+
+          <Button
+            color="primary"
+            onClick={() => console.log(time)}
+            variant="contained"
+          >
+            Show
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 const Contact = memo(ContactComponent);
 Contact.displayName = 'Contact';
